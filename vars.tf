a9975c58ff525f443573b65701054d314475031a
suresh <surya2fun@gmail.com>
2023-10-17 15:19:34 +0530

    terraform module roboshop

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "tags" {}
+variable "env" {}
+variable "lb_type" {}
+variable "internal" {}
+variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
+variable "sg_port" {}
+variable "subnets" {}
